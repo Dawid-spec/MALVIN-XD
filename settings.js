7c5fa15b221d43249c4639cbaedafb987e1822eb
Dawid-spec <botb05854@gmail.com>
2025-07-14 01:10:06 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "malvin~pzEyQKiI#MTye3kPdGjqSqHlvsSHIYQPXLLE1Cf8Cyr8Yrn6wZrw",
+SESSION_ID: process.env.SESSION_ID || "malvin~cqEHmZRD#6iRD9BXrjh-yqVl-89vLm7_WWCntoxpXXs2564jc0-4",
 // add your Session Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -27,7 +27,7 @@ LINK_WHITELIST: "youtube.com,github.com",
         AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 
-        AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+        AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 // make true if you want auto reply on status 
 
         AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*Just seen ur status 😆*",
